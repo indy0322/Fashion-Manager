8d9ffe2019557878322557790c6a4f3426479092
indy032229@gmail.com <indy032229@gmail.com>
2025-09-18 17:54:06 +0900

    스프링 부트 초기 프로젝트 디렉토리

---
+++ fashion-manager/src/main/java/fashionmanager/FashionManagerApplication.java
@@ -0,0 +1,13 @@
+package fashionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionManagerApplication.class, args);
+    }
+
+}
