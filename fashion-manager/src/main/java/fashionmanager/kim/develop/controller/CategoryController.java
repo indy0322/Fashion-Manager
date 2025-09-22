ebf49591586ecfe15c65698fda67b04bd89e0349
indy032229@gmail.com <indy032229@gmail.com>
2025-09-22 09:32:26 +0900

    컨트롤러 url 수정

--- fashion-manager/src/main/java/fashionmanager/kim/develop/controller/CategoryController.java
+++ fashion-manager/src/main/java/fashionmanager/kim/develop/controller/CategoryController.java
@@ -6,16 +6,14 @@ import fashionmanager.kim.develop.dto.ReviewCategoryDTO;
 import fashionmanager.kim.develop.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping(value = "/category")
 public class CategoryController {
 
@@ -27,185 +25,185 @@ public class CategoryController {
     }
 
     @GetMapping("/selectreviewcategory")
-    public List<ReviewCategoryDTO> selectAllReviewCategories() {
+    public ResponseEntity<List<ReviewCategoryDTO>> selectAllReviewCategories() {
         List<ReviewCategoryDTO> reviewCategoryList = cs.selectAllReviewCategories();
         for (ReviewCategoryDTO reviewCategoryDTO : reviewCategoryList) {
             log.info("reviewCategoryDTO: {}", reviewCategoryDTO);
         }
-        return reviewCategoryList;
+        return ResponseEntity.ok(reviewCategoryList);
     }
 
     @PostMapping("/insertreviewcategory")
-    public String insertReviewCategory(String insertReviewCategoryName) {
+    public ResponseEntity<String> insertReviewCategory(String insertReviewCategoryName) {
         List<ReviewCategoryDTO> list = cs.selectAllReviewCategories();
         for (ReviewCategoryDTO reviewCategoryDTO : list) {
             if(reviewCategoryDTO.getReviewCategoryName().equals(insertReviewCategoryName)){
                 log.info("새로운 후기 카테고리 요소 등록에 실패했습니다.");
-                return "새로운 후기 카테고리 요소 등록에 실패했습니다.";
+                return ResponseEntity.ok("새로운 후기 카테고리 요소 등록에 실패했습니다.");
             }
         }
 
         int result = cs.insertReviewCategory(insertReviewCategoryName);
         if(result == 1){
             log.info("새로운 후기 카테고리 " + insertReviewCategoryName + "요소가 등록되었습니다.");
-            return "새로운 후기 카테고리 " +insertReviewCategoryName + "요소가 등록되었습니다.";
+            return ResponseEntity.ok("새로운 후기 카테고리 " +insertReviewCategoryName + "요소가 등록되었습니다.");
         }else{
             log.info("새로운 후기 카테고리 요소 등록에 실패했습니다.");
-            return "새로운 후기 카테고리 요소 등록에 실패했습니다.";
+            return ResponseEntity.ok("새로운 후기 카테고리 요소 등록에 실패했습니다.");
         }
     }
 
     @PostMapping("updatereviewcategory")
-    public String updateReviewCategory(@RequestBody ReviewCategoryDTO reviewCategoryDTO) {
+    public ResponseEntity<String> updateReviewCategory(@RequestBody ReviewCategoryDTO reviewCategoryDTO) {
         List<ReviewCategoryDTO> list = cs.selectAllReviewCategories();
         for (ReviewCategoryDTO reviewCategory : list) {
             if(reviewCategory.getReviewCategoryName().equals(reviewCategoryDTO.getReviewCategoryName())){
                 log.info("새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 제대로 수정되지 못했습니다.");
-                return "새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 제대로 수정되지 못했습니다.";
+                return ResponseEntity.ok("새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 제대로 수정되지 못했습니다.");
             }
         }
 
         int result = cs.updateReviewCategory(reviewCategoryDTO);
         if(result == 1){
             log.info("새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 수정되었습니다.");
-            return "새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 수정되었습니다.";
+            return ResponseEntity.ok("새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 수정되었습니다.");
         }else{
             log.info("새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 제대로 수정되지 못했습니다.");
-            return "새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 제대로 수정되지 못했습니다.";
+            return ResponseEntity.ok("새로운 후기 카테고리 " + reviewCategoryDTO.getReviewCategoryName() + "요소가 제대로 수정되지 못했습니다.");
         }
     }
 
     @PostMapping("/deletereviewcategory")
-    public String deleteReviewCategory(int deleteReviewCategoryNum){
+    public ResponseEntity<String> deleteReviewCategory(int deleteReviewCategoryNum){
         int result = cs.deleteReviewCategory(deleteReviewCategoryNum);
         if(result == 1){
             log.info("후기 카테고리 " + deleteReviewCategoryNum + "번이 삭제되었습니다.");
-            return "후기 카테고리 " + deleteReviewCategoryNum + "번이 삭제되었습니다.";
+            return ResponseEntity.ok("후기 카테고리 " + deleteReviewCategoryNum + "번이 삭제되었습니다.");
         }else{
             log.info("후기 카테고리 " + deleteReviewCategoryNum + "번이 삭제에 실패했습니다.");
-            return "후기 카테고리 " + deleteReviewCategoryNum + "번이 삭제에 실패했습니다.";
+            return ResponseEntity.ok("후기 카테고리 " + deleteReviewCategoryNum + "번이 삭제에 실패했습니다.");
         }
     }
 
     @GetMapping("/selectreportcategory")
-    public List<ReportCategoryDTO> selectAllReportCategories() {
+    public ResponseEntity<List<ReportCategoryDTO>> selectAllReportCategories() {
         List<ReportCategoryDTO> reportCategoryList = cs.selectAllReportCategories();
         for (ReportCategoryDTO reportCategoryDTO : reportCategoryList) {
             log.info("reportCategoryDTO: {}", reportCategoryDTO);
         }
-        return reportCategoryList;
+        return ResponseEntity.ok(reportCategoryList);
     }
 
     @PostMapping("/insertreportcategory")
-    public String insertReportCategory(String insertReportCategoryName) {
+    public ResponseEntity<String> insertReportCategory(String insertReportCategoryName) {
         List<ReportCategoryDTO> list = cs.selectAllReportCategories();
         for (ReportCategoryDTO reportCategoryDTO : list) {
             if(reportCategoryDTO.getReportCategoryName().equals(insertReportCategoryName)){
                 log.info("새로운 신고 카테고리 요소 등록에 실패했습니다.");
-                return "새로운 신고 카테고리 요소 등록에 실패했습니다.";
+                return ResponseEntity.ok("새로운 신고 카테고리 요소 등록에 실패했습니다.");
             }
         }
 
         int result = cs.insertReportCategory(insertReportCategoryName);
         if(result == 1){
             log.info("새로운 신고 카테고리 " + insertReportCategoryName + "요소가 등록되었습니다.");
-            return "새로운 신고 카테고리 " +insertReportCategoryName + "요소가 등록되었습니다.";
+            return ResponseEntity.ok("새로운 신고 카테고리 " +insertReportCategoryName + "요소가 등록되었습니다.");
         }else{
             log.info("새로운 신고 카테고리 요소 등록에 실패했습니다.");
-            return "새로운 신고 카테고리 요소 등록에 실패했습니다.";
+            return ResponseEntity.ok("새로운 신고 카테고리 요소 등록에 실패했습니다.");
         }
     }
 
     @PostMapping("/updatereportcategory")
-    public String updateReportCategory(@RequestBody ReportCategoryDTO reportCategoryDTO) {
+    public ResponseEntity<String> updateReportCategory(@RequestBody ReportCategoryDTO reportCategoryDTO) {
         List<ReportCategoryDTO> list = cs.selectAllReportCategories();
         for (ReportCategoryDTO reportCategory : list) {
             if(reportCategory.getReportCategoryName().equals(reportCategoryDTO.getReportCategoryName())){
                 log.info("새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 제대로 수정되지 못했습니다.");
-                return "새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 제대로 수정되지 못했습니다.";
+                return ResponseEntity.ok("새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 제대로 수정되지 못했습니다.");
             }
         }
 
         int result = cs.updateReportCategory(reportCategoryDTO);
         if(result == 1){
             log.info("새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 수정되었습니다.");
-            return "새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 수정되었습니다.";
+            return ResponseEntity.ok("새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 수정되었습니다.");
         }else{
             log.info("새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 제대로 수정되지 못했습니다.");
-            return "새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 제대로 수정되지 못했습니다.";
+            return ResponseEntity.ok("새로운 신고 카테고리 " + reportCategoryDTO.getReportCategoryName() + "요소가 제대로 수정되지 못했습니다.");
         }
     }
 
     @PostMapping("/deletereportcategory")
-    public String deleteReportCategory(int deleteReportCategoryNum) {
+    public ResponseEntity<String> deleteReportCategory(int deleteReportCategoryNum) {
         int result = cs.deleteReportCategory(deleteReportCategoryNum);
         if(result == 1){
             log.info("신고 카테고리 " + deleteReportCategoryNum + "번이 삭제되었습니다.");
-            return "신고 카테고리 " + deleteReportCategoryNum + "번이 삭제되었습니다.";
+            return ResponseEntity.ok("신고 카테고리 " + deleteReportCategoryNum + "번이 삭제되었습니다.");
         }else{
             log.info("신고 카테고리 " + deleteReportCategoryNum + "번이 삭제에 실패했습니다.");
-            return "신고 카테고리 " + deleteReportCategoryNum + "번이 삭제에 실패했습니다.";
+            return ResponseEntity.ok("신고 카테고리 " + deleteReportCategoryNum + "번이 삭제에 실패했습니다.");
         }
     }
 
     @GetMapping("/selectmessagecategory")
-    public List<MessageCategoryDTO> selectAllMessageCategories() {
+    public ResponseEntity<List<MessageCategoryDTO>> selectAllMessageCategories() {
         List<MessageCategoryDTO> messageCategoryList = cs.selectAllMessageCategories();
         for (MessageCategoryDTO messageCategoryDTO : messageCategoryList) {
             log.info("messageCategoryDTO: {}", messageCategoryDTO);
         }
-        return messageCategoryList;
+        return ResponseEntity.ok(messageCategoryList);
     }
 
     @PostMapping("/insertmessagecategory")
-    public String insertMessageCategory(String insertMessageCategoryName) {
+    public ResponseEntity<String> insertMessageCategory(String insertMessageCategoryName) {
         List<MessageCategoryDTO> list = cs.selectAllMessageCategories();
         for (MessageCategoryDTO messageCategoryDTO : list) {
             if(messageCategoryDTO.getMessageCategoryName().equals(insertMessageCategoryName)){
                 log.info("새로운 쪽지 카테고리 요소 등록에 실패했습니다.");
-                return "새로운 쪽지 카테고리 요소 등록에 실패했습니다.";
+                return ResponseEntity.ok("새로운 쪽지 카테고리 요소 등록에 실패했습니다.");
             }
         }
 
         int result = cs.insertMessageCategory(insertMessageCategoryName);
         if(result == 1){
             log.info("새로운 쪽지 카테고리 " + insertMessageCategoryName + "요소가 등록되었습니다.");
-            return "새로운 쪽지 카테고리 " + insertMessageCategoryName + "요소가 등록되었습니다.";
+            return ResponseEntity.ok("새로운 쪽지 카테고리 " + insertMessageCategoryName + "요소가 등록되었습니다.");
         }else{
             log.info("새로운 쪽지 카테고리 요소 등록에 실패했습니다.");
-            return "새로운 쪽지 카테고리 요소 등록에 실패했습니다.";
+            return ResponseEntity.ok("새로운 쪽지 카테고리 요소 등록에 실패했습니다.");
         }
     }
 
     @PostMapping("/updatemessagecategory")
-    public String updateMessageCategory(@RequestBody MessageCategoryDTO messageCategoryDTO) {
+    public ResponseEntity<String> updateMessageCategory(@RequestBody MessageCategoryDTO messageCategoryDTO) {
         List<MessageCategoryDTO> list = cs.selectAllMessageCategories();
         for(MessageCategoryDTO messageCategory : list){
             if(messageCategory.getMessageCategoryName().equals(messageCategoryDTO.getMessageCategoryName())){
                 log.info("새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 제대로 수정되지 못했습니다.");
-                return "새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 제대로 수정되지 못했습니다.";
+                return ResponseEntity.ok("새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 제대로 수정되지 못했습니다.");
             }
         }
 
         int result = cs.updateMessageCategory(messageCategoryDTO);
         if(result == 1){
             log.info("새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 수정되었습니다.");
-            return "새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 수정되었습니다.";
+            return ResponseEntity.ok("새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 수정되었습니다.");
         }else{
             log.info("새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 제대로 수정되지 못했습니다.");
-            return "새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 제대로 수정되지 못했습니다.";
+            return ResponseEntity.ok("새로운 쪽지 카테고리 " + messageCategoryDTO.getMessageCategoryName() + "요소가 제대로 수정되지 못했습니다.");
         }
     }
 
     @PostMapping("/deletemessagecategory")
-    public String deleteMessageCategory(int deleteMessageCategoryNum) {
+    public ResponseEntity<String> deleteMessageCategory(int deleteMessageCategoryNum) {
         int result = cs.deleteMessageCategory(deleteMessageCategoryNum);
         if(result == 1){
             log.info("쪽지 카테고리 " + deleteMessageCategoryNum + "번이 삭제되었습니다.");
-            return "쪽지 카테고리 " + deleteMessageCategoryNum + "번이 삭제되었습니다.";
+            return ResponseEntity.ok("쪽지 카테고리 " + deleteMessageCategoryNum + "번이 삭제되었습니다.");
         }else{
             log.info("쪽지 카테고리 " + deleteMessageCategoryNum + "번이 삭제에 실패했습니다.");
-            return "쪽지 카테고리 " + deleteMessageCategoryNum + "번이 삭제에 실패했습니다.";
+            return ResponseEntity.ok("쪽지 카테고리 " + deleteMessageCategoryNum + "번이 삭제에 실패했습니다.");
         }
     }
 
--- fashion-manager/src/main/java/fashionmanager/kim/develop/controller/HashTagController.java
+++ fashion-manager/src/main/java/fashionmanager/kim/develop/controller/HashTagController.java
@@ -5,16 +5,18 @@ import fashionmanager.kim.develop.dto.HashTagDTO;
 import fashionmanager.kim.develop.service.HashTagService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
-@Controller
-@RequestMapping("/hashtage")
+@RestController
+@RequestMapping("/hashtag")
 public class HashTagController {
 
     private final HashTagService hs;
@@ -25,72 +27,72 @@ public class HashTagController {
     }
 
     @GetMapping("/selecthashtag")
-    public List<HashTagDTO> selectHashTags() {
+    public ResponseEntity<List<HashTagDTO>> selectHashTags() {
         List<HashTagDTO> hashTagList = hs.selectHashTags();
         for (HashTagDTO hashTagDTO : hashTagList) {
             log.info("hashTagDTO: {}", hashTagDTO);
         }
-        return hashTagList;
+        return ResponseEntity.ok(hashTagList);
     }
 
     @GetMapping("/selecthashtagandpost")
-    public List<HashTagAndPostDTO> selectHashTagsAndPosts() {
+    public ResponseEntity<List<HashTagAndPostDTO>> selectHashTagsAndPosts() {
         List<HashTagAndPostDTO> hashTagAndPostList = hs.selectHashTagsAndPosts();
         for (HashTagAndPostDTO hashTagAndPostDTO : hashTagAndPostList) {
             log.info("hashTagAndPostDTO: {}", hashTagAndPostDTO);
         }
-        return hashTagAndPostList;
+        return ResponseEntity.ok(hashTagAndPostList);
     }
 
     @PostMapping("/inserthashtag")
-    public String insertHashTag(String insertHashTagName) {
+    public ResponseEntity<String> insertHashTag(String insertHashTagName) {
         List<HashTagDTO> hashTagList = hs.selectHashTags();
         for (HashTagDTO hashTagDTO : hashTagList) {
             if(hashTagDTO.getHashTagName().equals(insertHashTagName)){
                 log.info("새로운 해시태그 등록에 실패했습니다.");
-                return "새로운 해시태그 등록에 실패했습니다.";
+                return ResponseEntity.ok("새로운 해시태그 등록에 실패했습니다.");
             }
         }
 
         int result = hs.insertHashTag(insertHashTagName);
         if(result == 1){
             log.info("새로운 해시태그 요소가 등록되었습니다.");
-            return "새로운 해시태그 요소가 등록되었습니다.";
+            return ResponseEntity.ok("새로운 해시태그 요소가 등록되었습니다.");
         }else{
             log.info("새로운 해시태그 등록에 실패했습니다.");
-            return "새로운 해시태그 등록에 실패했습니다.";
+            return ResponseEntity.ok("새로운 해시태그 등록에 실패했습니다.");
         }
     }
 
     @PostMapping("/updatehashtag")
-    public String updateHashTag(HashTagDTO updateHashTagDTO){
+    public ResponseEntity<String> updateHashTag(HashTagDTO updateHashTagDTO){
         List<HashTagDTO> hashTagList = hs.selectHashTags();
         for (HashTagDTO hashTagDTO : hashTagList) {
             if(hashTagDTO.getHashTagName().equals(updateHashTagDTO.getHashTagName())){
                 log.info("해시태그 수정에 실패했습니다.");
-                return "해시태그 수정에 실패했습니다.";
+                return ResponseEntity.ok("해시태그 수정에 실패했습니다.");
             }
         }
 
         int result = hs.updateHashTag(updateHashTagDTO);
         if(result == 1){
             log.info("해시태그 요소가 수정되었습니다.");
-            return "해시태그 요소가 수정되었습니다.";
+            return ResponseEntity.ok("해시태그 요소가 수정되었습니다.");
         }else{
             log.info("해시태그 수정에 실패했습니다.");
-            return "해시태그 수정에 실패했습니다.";
+            return ResponseEntity.ok("해시태그 수정에 실패했습니다.");
         }
     }
 
     @PostMapping("/deletehashtag")
-    public String deleteHashTag(int deleteHashTagNum){
+    public ResponseEntity<String> deleteHashTag(int deleteHashTagNum){
         int result = hs.deleteHashTag(deleteHashTagNum);
         if(result == 1){
             log.info("해시태그 요소가 삭제 되었습니다.");
-            return "해시태그 요소가 삭제 되었습니다.";
+            return ResponseEntity.ok("해시태그 요소가 삭제 되었습니다.");
         }else{
             log.info("해시태그 요소가 삭제에 실패했습니다.");
-            return "해시태그 요소가 삭제에 실패했습니다.";
+            return ResponseEntity.ok("해시태그 요소가 삭제에 실패했습니다.");
         }
     }
 }
