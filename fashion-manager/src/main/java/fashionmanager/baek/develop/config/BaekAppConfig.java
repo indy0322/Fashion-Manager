5667dfefad5e1b7b37806efe19d667d104ce7bc2
seoguhoe2 <js8337349@naver.com>
2025-09-26 11:06:30 +0900

    appconifg 이름 변경

--- fashion-manager/src/main/java/fashionmanager/baek/develop/config/BaekAppConfig.java
+++ fashion-manager/src/main/java/fashionmanager/baek/develop/config/BaekAppConfig.java
@@ -5,13 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-<<<<<<<< HEAD:fashion-manager/src/main/java/fashionmanager/baek/develop/config/AppConfig1.java
-public class AppConfig1 {
-========
 public class BaekAppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
->>>>>>>> features:fashion-manager/src/main/java/fashionmanager/baek/develop/config/BaekAppConfig.java
 }
