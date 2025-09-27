27ff6c282d5b43295bfa620cd928e74108b99056
seoguhoe2 <qorwnstmd@gmail.com>
2025-09-27 23:38:36 +0900

    #33 특정 게시물 조회 기능 구현         각각 Response 특화 미구현, 사진 미구현

--- fashion-manager/src/main/java/fashionmanager/baek/develop/dto/SelectDetailPostDTO.java
+++ fashion-manager/src/main/java/fashionmanager/baek/develop/dto/SelectDetailPostDTO.java
@@ -1,20 +1,25 @@
 package fashionmanager.baek.develop.dto;
 
 import fashionmanager.kim.develop.dto.HashTagDTO;
-import lombok.Data;
+import lombok.*;
 
 import java.util.List;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SelectDetailPostDTO {
     private int num;
     private String title;
     private String content;
-    private int member_num;
+    private int memberNum;
     private int good;
     private int cheer;
     private double temp;
+    private boolean finish;
     private List<ItemDTO> items;
-    private List<HashTagDTO> tags;
+    private List<HashTagDTO> hashtags;
     private List<PhotoDTO> photos;
 }
--- fashion-manager/src/main/java/fashionmanager/baek/develop/mapper/MentoringPostMapper.java
+++ fashion-manager/src/main/java/fashionmanager/baek/develop/mapper/MentoringPostMapper.java
@@ -1,6 +1,7 @@
 package fashionmanager.baek.develop.mapper;
 
 import fashionmanager.baek.develop.dto.SelectAllPostDTO;
+import fashionmanager.baek.develop.dto.SelectDetailPostDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 @Mapper
 public interface MentoringPostMapper {
     List<SelectAllPostDTO> findAll();
+
+    SelectDetailPostDTO findById(int postNum);
 }
