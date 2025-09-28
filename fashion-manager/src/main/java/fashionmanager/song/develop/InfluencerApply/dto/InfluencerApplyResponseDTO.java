36d0d522ae1dd6259021507f773c1d3713303158
thdgudtjr0415 <thdgudtjr0415@gmail.com>
2025-09-28 23:53:19 +0900

    InfluencerApply 기능 및 테스트 코드, 이미지 추가 #2

--- fashion-manager/src/main/java/fashionmanager/song/develop/InfluencerApply/dto/InfluencerApplyResponseDTO.java
+++ fashion-manager/src/main/java/fashionmanager/song/develop/InfluencerApply/dto/InfluencerApplyResponseDTO.java
@@ -2,6 +2,8 @@ package fashionmanager.song.develop.influencerApply.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -16,5 +18,8 @@ public class InfluencerApplyResponseDTO {
     private Integer memberNum;
 
     private String memberName;
+
+    //  이미지 관련 코드
+    private List<String> photoPaths;
 
 }
